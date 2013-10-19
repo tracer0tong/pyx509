d188afde1a1ff40d9195c73d187b4cddcb63b5b6
Ernesto Revilla <erevilla@yaco.es>
2013-10-19 17:07:24 +0200

    Cleanups
    * PEP8 / pyflakes
    * Raise error on unknown digest algorithm
    * Less code
    * Lazy b64 encoding on logging.

--- pkcs7/digest.py
+++ pkcs7/digest.py
@@ -18,40 +18,35 @@
 #*
 
 # standard library imports
+import base64
 import hashlib
 import logging
 logger = logging.getLogger("pkcs7.digest")
-import base64
 
 RSA_NAME = "RSA"
-SHA1_NAME = "SHA-1"
-SHA256_NAME = "SHA-256"
-SHA384_NAME = "SHA-384"
-SHA512_NAME = "SHA-512"
 
-def calculate_digest(data, alg):    
+
+class LazyB64:
+    '''
+    Lazy base 64 converter for logging.
+    '''
+
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return base64.b64encode(self.data)
+
+
+def calculate_digest(data, alg):
     '''
     Calculates digest according to algorithm
     '''
-    digest_alg = None
-    if (alg == SHA1_NAME):
-        digest_alg = hashlib.sha1() 
-    
-    if (alg == SHA256_NAME):
-        digest_alg = hashlib.sha256()
-    
-    if (alg == SHA384_NAME):
-        digest_alg = hashlib.sha384()
-    
-    if (alg == SHA512_NAME):
-        digest_alg = hashlib.sha512()
-    
-    if digest_alg is None:
-        logger.error("Unknown digest algorithm : %s" % alg)
-        return None
-    
-    digest_alg.update(data)   
-    dg = digest_alg.digest()       
-    
-    logger.debug("Calculated hash from input data: %s" % base64.b64encode(dg))    
-    return dg
+    try:
+        alg = alg.replace('-', '')
+        digest = hashlib.new(alg, data).digest()
+        logger.debug("Calculated hash from input data: %s", LazyB64(digest))
+        return digest
+    except ValueError:
+        logger.error("Unknown digest algorithm : %s", alg)
+        raise
